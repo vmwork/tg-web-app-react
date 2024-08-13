f9e723ce128a57edcbbb8b8878b63c47993c4362
V.Martseniuk <v.martseniuk@asbis.com>
2024-08-13 11:35:47 +0300

    fix button path

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "../Button/button";
+import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = (props) => {
