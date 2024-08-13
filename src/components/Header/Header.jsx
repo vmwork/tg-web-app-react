8894cfd15d146181ae4dd30626bd232015ddd5ec
V.Martseniuk <v.martseniuk@asbis.com>
2024-08-13 11:58:20 +0300

    get user

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <div className={"header"}>
       <Button onClick={onClose}>Закрыть</Button>
-      <span className={"username"}>{user?.userName}</span>
+      <span className={"username"}>{user}</span>
     </div>
   );
 };
