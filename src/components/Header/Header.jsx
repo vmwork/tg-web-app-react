b7475b47f1284d66a71a9b44e3dd64aea488118f
V.Martseniuk <v.martseniuk@asbis.com>
2024-09-08 12:09:35 +0300

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,6 @@ const Header = () => {
       <div
         className={"user-data flex flex-col items-center justify-items-center	"}
       >
-        <button onClick={onClose}>Закрыть</button>
         <img className="user-photo" src="/logo.webp" alt="" />
         <span className={"username"}>{user?.first_name}</span>
       </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import "./Main.css";
-
 import { useTelegram } from "../../hooks/useTelegram";
 import Button from "../Button/Button";
+
+const TELEGRAM_BOT_TOKEN = "7346201764:AAEHwMHDxpz1HrbBhuGS1gtQ47ACv9Q_31c";
+const TELEGRAM_CHAT_ID = "@testerRGBBotGROUPE";
+const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+
 const Main = () => {
   const { user } = useTelegram();
-  const sendAlert = () => {
+  const sendAlert = async () => {
     console.log("alert");
+    try {
+      const response = await fetch(API, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: TELEGRAM_CHAT_ID,
+          text: "text",
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   return (
     <div className={"main bg-color-main"}>
       <div className="main-wrapper">
-        <Button onclick={sendAlert} className={"pizdec"}>
+        <Button onClick={sendAlert} className={"pizdec"}>
           ТЦК
         </Button>
       </div>
