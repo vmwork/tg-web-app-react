9abf8a57c496178e95aba4851e6d11e70f99dad3
V.Martseniuk <v.martseniuk@asbis.com>
2024-09-08 14:34:27 +0300

    sac

--- src/hooks/useSendAlert.js
+++ src/hooks/useSendAlert.js
@@ -1,11 +1,12 @@
 import { useSendMessage } from "./useSendMessage";
 import { useSendPhoto } from "./useSendPhoto";
 import { useSendLocation } from "./useSendLocation";
-export function useSendAlert(data) {
+export function useSendAlert() {
   const { sendMessage } = useSendMessage();
   const { sendPhoto } = useSendPhoto();
   const { sendLocation } = useSendLocation();
   const sendAlert = async (user) => {
+    console.log(user);
     await sendPhoto();
     await sendMessage(user);
     await sendLocation();
